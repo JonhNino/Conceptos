9b0d5c4e3c0c43b61980da14209181d65e57ccb5
Jonh Nino <jonhninoa@gmail.com>
2023-09-01 20:50:24 -0500

    Init

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import com.uptc.model.AutorEntity;
+
+public class Main {
+    public static void main(String[] args) {
+        AutorEntity autorEntity = new AutorEntity();
+    }
+}
---
+++ src/com/uptc/model/AutorEntity.java
@@ -0,0 +1,8 @@
+package com.uptc.model;
+
+public class AutorEntity {
+
+    private Long id;
+    private String nombre;
+    private String correo;
+}
